4b5b83db49466aefb8108f46d78018de84673a03
studyinglover <3046308220@qq.com>
2023-01-19 20:47:53 +0800

    fix mistake

--- src/map.js
+++ src/map.js
@@ -1,11 +1,10 @@
 // sleep 10s
-setTimeout(function() {
-    
-    while (true){
-        // console.log(playerMesh.position);
+setTimeout(function () {
 
-    }
-  }, 4000);
+    console.log(playerMesh.position);
 
 
-  
+}, 4000);
+
+
+
